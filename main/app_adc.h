255171280c315678ff2d29c266f54f8862efd8eb
Mouad <rinmodokaty@gmail.com>
2021-04-20 17:09:19 +0000

    Add files via upload

---
+++ main/app_adc.h
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_spi_flash.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_partition.h"
+#include "driver/i2s.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+#include "esp_task_wdt.h"
+
+//#if CONFIG_IDF_TARGET_ESP32
+
+static const char* TAG = "ad/da";
+#define V_REF   1100
+#define ADC1_TEST_CHANNEL (ADC1_CHANNEL_5)
+
+#define PARTITION_NAME   "storage"
+
+/*---------------------------------------------------------------
+                            EXAMPLE CONFIG
+---------------------------------------------------------------*/
+//enable record sound and save in flash
+
+#define RECORD_IN_FLASH_EN        (1)
+//enable replay recorded sound in flash
+#define REPLAY_FROM_FLASH_EN      (1)
+
+//i2s number
+#define EXAMPLE_I2S_NUM           (0)
+//i2s sample rate
+#define EXAMPLE_I2S_SAMPLE_RATE   (25600)
+//i2s data bits
+#define EXAMPLE_I2S_SAMPLE_BITS   (16)
+//enable display buffer for debug
+#define EXAMPLE_I2S_BUF_DEBUG     (0)
+//I2S read buffer length (16*1024)
+#define EXAMPLE_I2S_READ_LEN      (16*1024)
+//I2S data format : I2S_CHANNEL_FMT_RIGHT_LEFT
+#define EXAMPLE_I2S_FORMAT        (I2S_CHANNEL_FMT_RIGHT_LEFT)
+//I2S channel number
+#define EXAMPLE_I2S_CHANNEL_NUM   ((EXAMPLE_I2S_FORMAT < I2S_CHANNEL_FMT_ONLY_RIGHT) ? (2) : (1))
+//I2S built-in ADC unit
+#define I2S_ADC_UNIT              ADC_UNIT_1
+//I2S built-in ADC channel
+#define I2S_ADC_CHANNEL           ADC1_CHANNEL_5
+
+//flash record size, for recording 5 seconds' data
+#define FLASH_RECORD_SIZE         (EXAMPLE_I2S_CHANNEL_NUM * EXAMPLE_I2S_SAMPLE_RATE * EXAMPLE_I2S_SAMPLE_BITS / 8 * 5)
+#define FLASH_ERASE_SIZE          (FLASH_RECORD_SIZE % FLASH_SECTOR_SIZE == 0) ? FLASH_RECORD_SIZE : FLASH_RECORD_SIZE + (FLASH_SECTOR_SIZE - FLASH_RECORD_SIZE % FLASH_SECTOR_SIZE)
+//sector size of flash
+#define FLASH_SECTOR_SIZE         (0x1000)
+//flash read / write address
+#define FLASH_ADDR                (0x200000)
+
+extern void example_i2s_init(void);
+extern void example_disp_buf(uint8_t* buf, int length);
+extern void example_i2s_adc_dac(void*arg);
+extern void adc_read_task(void* arg);
+
+#define do_sending  1
+
